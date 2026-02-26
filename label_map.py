5a1e2a26da718529aebedc443e539a6c9db2e051
hle357-hub <hle623626@gmail.com>
2026-02-26 20:32:59 +0700

    Export label_map to JSON file
    Added functionality to export label_map to a JSON file.

--- label_map.py
+++ label_map.py
@@ -1,10 +1,13 @@
-# label_map.py
-'''
-buoc 6
-'''
+import json
+
 label_map = {
     0: "zebra",
     1: "wine_bottle",
     2: "chair",
     3: "window"
 }
+
+with open(r"D:\code\python\ttcs\label_map.json", "w") as f:
+    json.dump(label_map, f, indent=4)
+
+print("Đã tạo file label_map.json")
